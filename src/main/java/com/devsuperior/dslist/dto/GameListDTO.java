315008c2949dd70964cffacafc88a85de6f19245
Gabriel Florentino <gabriel.florentino@discente.ufg.br>
2024-11-16 16:18:10 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,23 @@
+ package com.devsuperior.dslist.dto;
+
+import java.util.Objects;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+}
