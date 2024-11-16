95620ab183bc61fe3440b4b81730838be84c989a
Gabriel Florentino <gabriel.florentino@discente.ufg.br>
2024-11-16 12:41:55 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,23 +22,29 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longtDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game() {	
 	}
 
 	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longtDescription) {
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longtDescription = longtDescription;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -81,6 +87,13 @@ public class Game {
 		this.platforms = platforms;
 	}
 
+	public Double getScore() {
+		return score;
+	}
+	
+	public void setScore(Double score) {
+		this.score = score;
+	}
 	public String getImgUrl() {
 		return imgUrl;
 	}
@@ -97,12 +110,12 @@ public class Game {
 		this.shortDescription = shortDescription;
 	}
 
-	public String getLongtDescription() {
-		return longtDescription;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongtDescription(String longtDescription) {
-		this.longtDescription = longtDescription;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
